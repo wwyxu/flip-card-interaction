54b4f3fb4fb28548a016655b4fc3b5620fb1a542
William Xu <william-xu@live.com>
2021-07-08 18:24:12 +1000

    fix

--- src/views/info-card/index.tsx
+++ src/views/info-card/index.tsx
@@ -6,15 +6,15 @@ interface Info extends Models.Back {
   type: string;
 }
 
-const InfoCard = ({asset, title, description, type}: Info) => {
+const InfoCard = ({ asset, title, description, type }: Info) => {
   return (
     <div className={`container info-card ${type} rounded text-white my-3 p-4 `}>
       <div className="row">
         <div className="col-md-2">
-          <div style={{textAlign: "center"}}>
-          <img className="center ml-2" style={{display: "inline-block"}} src={asset} alt="intro" height="100" />
+          <div style={{ textAlign: "center" }}>
+            <img className="center ml-2" style={{ display: "inline-block" }} src={asset} alt="intro" height="100" />
           </div>
-          </div>
+        </div>
         <div className="col-md-10">
           <h5 className="mb-2">{title}</h5>
           <p className="small-font">{concatDescription(description)}</p>
